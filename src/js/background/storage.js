0c15b6d5cddf7ecf0b77d1998c4834b5b6cbd5a6
zqbright <zqbright@gmail.com>
2014-08-19 19:48:22 +0800

    commit some files

--- src/js/background/storage.js
+++ src/js/background/storage.js
@@ -2,7 +2,36 @@
 var storage = {
 	_storage:chrome.storage,
 	l_storage:_storage.local,
+	_getEmptyFun:function(){
+		return function(){};
+	}
 	set:function(obj,cb){
+
+		cb = cb || _getEmptyFun;
 		l_storage.set(obj,cb)	
-	}	
+	},
+	get:function(key,cb){
+
+		cb = cb || _getEmptyFun;
+		l_storage.get(key,cb);
+	},
+	getUsed:function(key,cb){
+
+		cb = cb || _getEmptyFun;
+		l_storage.getBytesInUse(key,cb);
+	},
+	remove:function(key){
+
+		cb = cb || _getEmptyFun;
+		l_storage.remove(key,cb);
+	},
+	clear:function(cb){
+
+		cb = cb || _getEmptyFun;
+		_storage.clear(cb);
+	}
+	bindChange:function(cb){
+		cb = cb || _getEmptyFun;
+		l_storage.onChanged.addListener(cb);
+	}
 }
